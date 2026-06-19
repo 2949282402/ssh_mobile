d6a121f49196a9cdbcb45507a65589370a080038
Julian <2949282402@qq.com>
2026-06-20 00:36:03 +0800

    chore: upgrade gradle, android build tools, and dependencies
    - Upgrade gradle wrapper to 8.14.5 using Tencent Cloud mirror.
    - Upgrade Android Gradle Plugin to 8.11.1 and apply Flutter migrator flags.
    - Update Dart and Flutter SDK constraints in pubspec.lock and update dependencies.
    - Import cupertino.dart in app_theme.dart.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:animations/animations.dart';
 
 class ExtendedColors extends ThemeExtension<ExtendedColors> {
