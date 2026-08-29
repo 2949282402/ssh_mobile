3f0c8c62e43a3a02e6e9218c862aae414df7bd58
hejulian <2949282402@qq.com>
2026-08-29 11:49:22 +0800

    chore(client): format ssh session telemetry test

--- apps/ssh_mobile_full/test/services/telemetry/ssh_session_telemetry_test.dart
+++ apps/ssh_mobile_full/test/services/telemetry/ssh_session_telemetry_test.dart
@@ -93,9 +93,11 @@ void main() {
             host: '127.0.0.1',
             port: 22,
             expectedAlgorithm: 'ssh-ed25519',
-            expectedFingerprint: 'MD5:00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f',
+            expectedFingerprint:
+                'MD5:00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f',
             actualAlgorithm: 'ssh-ed25519',
-            actualFingerprint: 'MD5:10:11:12:13:14:15:16:17:18:19:1a:1b:1c:1d:1e:1f',
+            actualFingerprint:
+                'MD5:10:11:12:13:14:15:16:17:18:19:1a:1b:1c:1d:1e:1f',
           ),
         ),
         peerIdResolver: _enrolledPeerId,
